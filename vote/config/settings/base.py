2655c18587c292a05ad0318ea28ed02952b103fd
ktbm <boliventtalla12@gmail.com>
2026-05-04 11:18:19 +0100

    update

--- vote/config/settings/base.py
+++ vote/config/settings/base.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = BASE_DIR / "vote"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
-if READ_DOT_ENV_FILE:
-    # OS environment variables take precedence over variables from .env
-    env.read_env(str(BASE_DIR / ".env"))
+# Always read .env file if it exists
+env_file = BASE_DIR / ".env"
+if env_file.exists():
+    env.read_env(str(env_file))
 
 # GENERAL
 # ------------------------------------------------------------------------------
